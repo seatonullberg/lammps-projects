e737c92fa37e78a90ff4c45f49e91da3038ac738
Seaton Ullberg <seatonullberg@gmail.com>
2020-08-14 00:48:02 -0400

    Add more BCC structures

--- calculations/elastic-tensor/configuration.py
+++ calculations/elastic-tensor/configuration.py
@@ -17,57 +17,9 @@ STRUCTURES_DIRECTORY = os.path.join(
 CONFIGURATION = {}
 
 #####################
-#  Fe FCC SETTINGS  #
+#  Fe BCC SETTINGS  #
 #####################
-# CONFIGURATION["atom_types"] = "Fe"
-# CONFIGURATION["potential_path"] = os.path.join(POTENTIALS_DIRECTORY, "FeCrNi_FCC_Zhou_2018.eam.alloy")
-# CONFIGURATION["pair_style"] = "eam/alloy"
-# CONFIGURATION["structure_path"] = os.path.join(STRUCTURES_DIRECTORY, "Fe_FCC_bulk_1x1x1.lmp")
-
-###########################
-#  Fe75Cr25 FCC SETTINGS  #
-###########################
-# CONFIGURATION["atom_types"] = "Cr Fe"
-# CONFIGURATION["potential_path"] = os.path.join(POTENTIALS_DIRECTORY, "FeCrNi_FCC_Zhou_2018.eam.alloy")
-# CONFIGURATION["pair_style"] = "eam/alloy"
-# CONFIGURATION["structure_path"] = os.path.join(STRUCTURES_DIRECTORY, "Fe75Cr25_FCC_bulk_5x5x5.lmp")
-
-###########################
-#  Fe75Cr25 BCC SETTINGS  #
-###########################
-# CONFIGURATION["atom_types"] = "Cr Fe"
-# CONFIGURATION["potential_path"] = os.path.join(POTENTIALS_DIRECTORY, "FeCr_BCC_Stukowski_2009.cdeam")
-# CONFIGURATION["pair_style"] = "eam/cd"
-# CONFIGURATION["structure_path"] = os.path.join(STRUCTURES_DIRECTORY, "Fe75Cr25_BCC_bulk_5x5x5.lmp")
-
-###########################
-#  Fe50Cr50 FCC SETTINGS  #
-###########################
-# CONFIGURATION["atom_types"] = "Cr Fe"
-# CONFIGURATION["potential_path"] = os.path.join(POTENTIALS_DIRECTORY, "FeCrNi_FCC_Zhou_2018.eam.alloy")
-# CONFIGURATION["pair_style"] = "eam/alloy"
-# CONFIGURATION["structure_path"] = os.path.join(STRUCTURES_DIRECTORY, "Fe50Cr50_FCC_bulk_5x5x5.lmp")
-
-###########################
-#  Fe50Cr50 BCC SETTINGS  #
-###########################
-# CONFIGURATION["atom_types"] = "Cr Fe"
-# CONFIGURATION["potential_path"] = os.path.join(POTENTIALS_DIRECTORY, "FeCr_BCC_Stukowski_2009.cdeam")
-# CONFIGURATION["pair_style"] = "eam/cd"
-# CONFIGURATION["structure_path"] = os.path.join(STRUCTURES_DIRECTORY, "Fe50Cr50_BCC_bulk_5x5x5.lmp")
-
-###########################
-#  Fe25Cr75 FCC SETTINGS  #
-###########################
-# CONFIGURATION["atom_types"] = "Cr Fe"
-# CONFIGURATION["potential_path"] = os.path.join(POTENTIALS_DIRECTORY, "FeCrNi_FCC_Zhou_2018.eam.alloy")
-# CONFIGURATION["pair_style"] = "eam/alloy"
-# CONFIGURATION["structure_path"] = os.path.join(STRUCTURES_DIRECTORY, "Fe25Cr75_FCC_bulk_5x5x5.lmp")
-
-###########################
-#  Fe75Cr25 BCC SETTINGS  #
-###########################
 CONFIGURATION["atom_types"] = "Cr Fe"
 CONFIGURATION["potential_path"] = os.path.join(POTENTIALS_DIRECTORY, "FeCr_BCC_Stukowski_2009.cdeam")
 CONFIGURATION["pair_style"] = "eam/cd"
-CONFIGURATION["structure_path"] = os.path.join(STRUCTURES_DIRECTORY, "Fe25Cr75_BCC_bulk_5x5x5.lmp")
+CONFIGURATION["structure_path"] = os.path.join(STRUCTURES_DIRECTORY, "Fe75Cr25_BCC_bulk_5x5x5.lmp")
